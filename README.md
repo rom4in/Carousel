f3fc09cfb6cf10d21b7d242e1e35e474720658e1
Romain <rom4in@me.com>
2023-05-16 19:16:41 +0100

    ship it

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Carousel
 
-A description of this package.
+Implementing a snapping carousel in #SwiftUI. Surprisingly (to me), none of the existing carousels I found use `ScrollView`. It looks like the trick is to use `HStack`, `DragGesture` and `offset(x:)`.
+
+What do you know, it works great.
+@jarwarren1
--- Sources/Carousel/Carousel.swift
+++ Sources/Carousel/Carousel.swift
@@ -1,6 +1,54 @@
-public struct Carousel {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct Carousel<Data, Content>: View where Data: RandomAccessCollection, Data.Index: Hashable, Content: View {
+    @State private var currentIndex = 0
+    @State private var offsetX: CGFloat = 0
+
+    let items: Data
+    let itemWidth: CGFloat
+    let content: (Data.Element) -> Content
+    var onIndexChanged: (Int) -> Void
+
+    public init(items: Data, itemWidth: CGFloat, @ViewBuilder content: @escaping (Data.Element) -> Content, onIndexChanged: @escaping (Int) -> Void) {
+        self.items = items
+        self.itemWidth = itemWidth
+        self.content = content
+        self.onIndexChanged = onIndexChanged
+    }
+
+    public var body: some View {
+        GeometryReader { geometry in
+            HStack(alignment: .center, spacing: 0) {
+                ForEach(items.indices, id: \.self) { index in
+                    content(items[index])
+                        .frame(width: itemWidth)
+                }
+            }
+            .padding(.horizontal, (geometry.size.width - itemWidth) / 2)
+            .offset(x: offsetX)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        offsetX = value.translation.width - CGFloat(currentIndex) * itemWidth
+                    }
+                    .onEnded { value in
+                        let speedThreshold: CGFloat = 100 // if the final velocity is below threshold, treat as a drag (snap in place) instead of swipe
+                        if abs(value.predictedEndLocation.x - value.location.x) < speedThreshold {
+                            let indexOffset = Int(round(offsetX / -itemWidth))
+                            currentIndex = max(0, min(items.count - 1, indexOffset))
+                        } else {
+                            let predictedEndOffset = value.predictedEndTranslation.width + offsetX
+                            let predictedIndexOffset = Int(round(predictedEndOffset / -itemWidth))
+                            currentIndex = max(0, min(items.count - 1, predictedIndexOffset))
+                        }
+                        withAnimation(.spring()) {
+                            offsetX = CGFloat(currentIndex) * -itemWidth
+                        }
+                        onIndexChanged(currentIndex)
+                    }
+            )
+        }
     }
 }
+
+
